d06821461c7b5bfb4a093952f7b675af7c9622c0
gitsam35 <1587945769@qq.com>
2020-11-25 16:40:52 +0800

    新增几个页面

--- 仿京东商城-js/js/index.js
+++ 仿京东商城-js/js/index.js
@@ -17,7 +17,7 @@ window.addEventListener('load', function() {
         timer = setInterval(function() {
             //手动调用点击事件
             arrow_r.click();
-        }, 2000);
+        }, 3000); // 控制鼠标经过离开，3s后自动轮播
     });
     // 3. 动态生成小圆圈  有几张图片，我就生成几个小圆圈
     var ul = focus.querySelector('ul');
@@ -30,7 +30,7 @@ window.addEventListener('load', function() {
         li.setAttribute('index', i);
         // 把小li插入到ol 里面
         ol.appendChild(li);
-        // 4. 小圆圈的排他思想 我们可以直接在生成小圆圈的同时直接绑定点击事件
+        // 4. 小圆圈的排他思想 可以直接在生成小圆圈的同时直接绑定点击事件
         li.addEventListener('click', function() {
             // 干掉所有人 把所有的小li 清除 current 类名
             for (var i = 0; i < ol.children.length; i++) {
@@ -40,16 +40,13 @@ window.addEventListener('load', function() {
             this.className = 'current';
             // 5. 点击小圆圈，移动图片 当然移动的是 ul 
             // ul 的移动距离 小圆圈的索引号 乘以 图片的宽度 注意是负值
-            // 当我们点击了某个小li 就拿到当前小li 的索引号
+            // 当点击了某个小li 就拿到当前小li 的索引号
             var index = this.getAttribute('index');
-            // 当我们点击了某个小li 就要把这个li 的索引号给 num  
+            // 当点击了某个小li 就要把这个li 的索引号给 num  
             num = index;
-            // 当我们点击了某个小li 就要把这个li 的索引号给 circle  
+            // 当点击了某个小li 就要把这个li 的索引号给 circle  
             circle = index;
             // num = circle = index;
-            console.log(focusWidth);
-            console.log(index);
-
             animate(ul, -index * focusWidth);
         })
     }
@@ -67,7 +64,7 @@ window.addEventListener('load', function() {
     arrow_r.addEventListener('click', function() {
         if (flag) {
             flag = false; // 关闭节流阀
-            // 如果走到了最后复制的一张图片，此时 我们的ul 要快速复原 left 改为 0
+            // 如果走到了最后复制的一张图片，此时 的ul 要快速复原 left 改为 0
             if (num == ul.children.length - 1) {
                 ul.style.left = 0;
                 num = 0;
@@ -78,7 +75,7 @@ window.addEventListener('load', function() {
             });
             // 8. 点击右侧按钮，小圆圈跟随一起变化 可以再声明一个变量控制小圆圈的播放
             circle++;
-            // 如果circle == 4 说明走到最后我们克隆的这张图片了 我们就复原
+            // 如果circle == 4 说明走到最后克隆的这张图片了 就复原
             if (circle == ol.children.length) {
                 circle = 0;
             }
